560b8e201fc62556225f6b33008aa31d9da0ab53
Charles <because@charlesis.me>
2017-12-16 12:11:13 -0600

    Commit 2 - It's all about Me done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+/*
 
+<!--             Clicker         -->
 
 let clicker = {
     clickCount: 0,
@@ -9,3 +11,25 @@ let clicker = {
     }
     
 }
+
+*/
+
+let myInformation = {
+    name: "Charles McGregory",
+    age: 26,
+    tidbit: "When no one is around, I lay on the floor and pretend to be a discarded sock.",
+    statement: function() {
+    let aged = myInformation.age + 10;
+    console.log("Greetings random person, my government name is " + myInformation.name + " but they call me Petty Rosevelt out here in these screets, you feel me? " +myInformation.tidbit + " and in ten years I'll be " + aged + ".")
+},
+    timeWarp: function() {
+        myInformation.age ++
+    },
+    fbi: function(protection) {
+    myInformation.name = protection;
+}
+}
+myInformation.fbi("Kittens")
+myInformation.timeWarp()
+console.log(myInformation.age)
+console.log(myInformation.name)
