a17fe2fe6074bc67d9ed8bfc08e3b560bd0d2bd3
Charles <because@charlesis.me>
2017-12-18 17:12:01 -0600

    Commit 12 - Made a Tomagotchi object

---
+++ js/iterator_answers.js
@@ -0,0 +1,6 @@
+let words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"]
+
+
+words.forEach(function (keys) {
+    console.log (words[keys].toUpperCase())
+})
---
+++ tomagotchi/js/app.js
@@ -0,0 +1,15 @@
+//systems
+
+//emotions
+
+//build
+let tomagotchi = {
+    name: "Shinron",
+    creatureType: "dragon",
+    foodInTummy: 10,
+    restedness: 10,
+    health: 10
+    cry: function() {
+    console.log("WAAAAAA!!!")
+}
+}
