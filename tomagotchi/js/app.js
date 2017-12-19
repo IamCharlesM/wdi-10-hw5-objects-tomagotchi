b4053112bf515d24fa380c563df0f1e32ef0faa3
Charles <because@charlesis.me>
2017-12-19 09:06:52 -0600

    I don't know what was completed, GIT status just told me I had updates.

--- tomagotchi/js/app.js
+++ tomagotchi/js/app.js
@@ -5,20 +5,20 @@
 //build
 let tomagotchi = {
     name: "Shinron",
-    creatureType: "dragon",
+    creatureType: "Orb",
     foodInTummy: 10,
     restedness: 10,
     health: 10,
-    cry: function () {
-        this.foodInTummy--
-            console.log("WAAAAAA!!!" + this.name + " is getting hungery and only has " + this.foodInTummy + " food left")
-
-    },
-    puke: function () {
+    cry: function() {
+        this.foodInTummy --
+    console.log("WAAAAAA!!!" + this.name + " is getting hungery and only has " + this.foodInTummy + " food left")
+        
+},
+    puke: function() {
         console.log(this.name + " doesn't feel well: Bleeeh :0~~")
     },
-    yawn: function () {
+    yawn: function() {
         console.log(this.name + " goes 'Yaaaawwwnnn'")
     },
-
+    
 }
